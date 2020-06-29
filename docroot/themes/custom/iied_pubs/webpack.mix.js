0f63401abd96eea55750b3b362c778eb30e2f958
Chris Brody <chris@cbrody.com>
2020-06-29 14:43:22 +0100

    Update theme config

--- docroot/themes/custom/iied_pubs/webpack.mix.js
+++ docroot/themes/custom/iied_pubs/webpack.mix.js
@@ -7,7 +7,7 @@
  | for your application. See https://github.com/JeffreyWay/laravel-mix.
  |
  */
-const proxy = 'http://drupal.local';
+const proxy = 'http://pubs.dd:8083/';
 const mix = require('laravel-mix');
 
 /*
