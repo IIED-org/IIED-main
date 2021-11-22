9586b9aa6a8ad434e94a920f93d395c256560f1b
Finn <finn@agile.coop>
2021-11-22 21:58:35 +0000

    Create work in progress stub migration module for iied_migrate_d7 content.

---
+++ docroot/modules/custom/iied_migrate_d7/src/Plugin/migrate/source/D7Project.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\iied_migrate_d7\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\node\Plugin\migrate\source\d7\NodeComplete;
+use Drupal\migrate\Row;
+
+/**
+ * Source plugin for project content.
+ *
+ * This is a useful sub-class of NodeComplete to allow us to override the query
+ * or alter the data in prepareRow().
+ *
+ * @MigrateSource(
+ *   id = "iied_d7_project",
+ *   source_module = "node"
+ * )
+ */
+class D7Project extends NodeComplete {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = parent::query();
+    //$query->orderBy('nr.vid', 'DESC');
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $c = '';
+    return parent::prepareRow($row);
+  }
+
+}
