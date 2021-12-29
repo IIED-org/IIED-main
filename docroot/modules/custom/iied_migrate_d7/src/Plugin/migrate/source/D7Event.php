7cd8072c8ffb8cbde9f25a7ac64395de3cb7cb26
Finn <finn@agile.coop>
2021-12-29 22:49:28 +0000

    Update event content type and add iied_d7_events migration.

---
+++ docroot/modules/custom/iied_migrate_d7/src/Plugin/migrate/source/D7Event.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\iied_migrate_d7\Plugin\migrate\source;
+
+use Drupal\node\Plugin\migrate\source\d7\Node;
+
+/**
+ * Source plugin for event content.
+ *
+ * @MigrateSource(
+ *   id = "iied_d7_event",
+ *   source_module = "node"
+ * )
+ */
+class D7Event extends Node {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = parent::query();
+    // Select only source nodes that are published.
+    $query->condition('n.status', 1);
+    // $query->condition('n.nid', '139731');
+    return $query;
+  }
+
+}
