eee98d8ac7d8a1a8dd83d24e35e579e322c8fb35
cbrody <chris@cbrody.com>
2022-02-09 12:31:30 +0000

    Add callisto-theme

---
+++ docroot/themes/custom/callisto-theme/source/components/01-foundation/01-global/global-includes.js
@@ -0,0 +1,12 @@
+/* Reserving this file for global utility includes */
+
+/**
+ * @fileoverview
+ * @suppress {uselessCode|checkTypes}
+ */
+
+// https://github.com/jonathantneal/svg4everybody
+/* SVG polyfill so that our SVGs work on IE11 too */
+const svg4everybody = require('svg4everybody');
+
+svg4everybody();
