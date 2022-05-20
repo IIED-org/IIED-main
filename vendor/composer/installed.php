0c1b5e18d739639a2aa33920b4600c6b4039cf37
Chris Brody <chris.brody@iied.org>
2022-05-20 14:15:37 +0100

    Install and configure entityqueue

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '123edea0c9031b6205b1de3d8d92b1deb7332c43',
+        'reference' => 'da9f4a82201d803c99f574fbb90b63fd8a21d180',
         'name' => 'drupal/recommended-project',
         'dev' => true,
     ),
@@ -1057,6 +1057,15 @@
             'reference' => '8.x-1.9',
             'dev_requirement' => false,
         ),
+        'drupal/entityqueue' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'type' => 'drupal-module',
+            'install_path' => __DIR__ . '/../../docroot/modules/contrib/entityqueue',
+            'aliases' => array(),
+            'reference' => '8.x-1.2',
+            'dev_requirement' => false,
+        ),
         'drupal/eu_cookie_compliance' => array(
             'pretty_version' => '1.19.0',
             'version' => '1.19.0.0',
@@ -1752,7 +1761,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '123edea0c9031b6205b1de3d8d92b1deb7332c43',
+            'reference' => 'da9f4a82201d803c99f574fbb90b63fd8a21d180',
             'dev_requirement' => false,
         ),
         'drupal/redirect' => array(
