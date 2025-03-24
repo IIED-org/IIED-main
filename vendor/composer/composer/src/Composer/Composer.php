e1b896c111336dfe8900f58b28c21b95db12ef3c
Luke <luke@agile.coop>
2025-03-24 13:53:49 +0000

    core update

--- vendor/composer/composer/src/Composer/Composer.php
+++ vendor/composer/composer/src/Composer/Composer.php
@@ -51,9 +51,9 @@ class Composer extends PartialComposer
      *
      * @see getVersion()
      */
-    public const VERSION = '2.8.5';
+    public const VERSION = '2.8.6';
     public const BRANCH_ALIAS_VERSION = '';
-    public const RELEASE_DATE = '2025-01-21 15:23:40';
+    public const RELEASE_DATE = '2025-02-25 13:03:50';
     public const SOURCE_VERSION = '';
 
     /**
