fbeaa74ae421581a7076a2977f070bc5aee61d82
cbrody <chris.brody@iied.org>
2026-06-04 15:07:15 +0100

    Clean vendor dir

--- vendor/palantirnet/drupal-rector/fixtures/d9/rector_examples_updated/user_functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function user_functions() {
-    // user_password().
-    $password = \Drupal::service('password_generator')->generate();
-    $other_password = \Drupal::service('password_generator')->generate(8);
-    $password_length = 12;
-    $last_password = \Drupal::service('password_generator')->generate($password_length);
-}
