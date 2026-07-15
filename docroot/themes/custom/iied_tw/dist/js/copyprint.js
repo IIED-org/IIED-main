b46e9ca296aba31defc1be2a8d7f8c452b53c7a7
cbrody <chris@cbrody.com>
2026-07-15 17:04:12 +0100

    Update copy/print twig and js to use class

--- docroot/themes/custom/iied_tw/dist/js/copyprint.js
+++ docroot/themes/custom/iied_tw/dist/js/copyprint.js
@@ -1,19 +1,21 @@
-copyUrl = document.querySelector('#copyUrl');
-copyUrl.addEventListener('click', function(event) {
-  try {
-    let successful = navigator.clipboard.writeText(window.location.href);
-    let msg = successful ? 'successful' : 'unsuccessful';
-    alert('Copy link command was ' + msg);
-  } catch(err) {
-    alert('Unable to copy');
-  }
+
+document.querySelectorAll('.js-copy-url').forEach(function(copyUrl) {
+  copyUrl.addEventListener('click', async function(event) {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      alert('Link copied to clipboard');
+    } catch (err) {
+      alert('Unable to copy link');
+    }
+  });
 });
 
-printPdf = document.querySelector('#printPdf');
-printPdf.addEventListener('click', function(event) {
-  try {
-    window.print();
-  } catch(err) {
-    alert('Unable to print page');
-  }
+document.querySelectorAll('.js-print-pdf').forEach(function(printPdf) {
+  printPdf.addEventListener('click', function(event) {
+    try {
+      window.print();
+    } catch (err) {
+      alert('Unable to print page');
+    }
+  });
 });
